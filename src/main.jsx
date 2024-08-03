bfd353e6a22c5d5fd2be9b59669004a5d1ae48b6
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-03 23:19:40 +0330

    react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
