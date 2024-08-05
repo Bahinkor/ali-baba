53090d54895e0ae89b66e6c920ad3341214e5b00
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-06 00:10:07 +0330

    Development products box component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import ProductsBox from "../../Component/ProductsBox/ProductsBox.jsx";
+
 export default function Home() {
     //JSX
     return (
@@ -6,6 +8,10 @@ export default function Home() {
                 <div>
                     <img src="https://cdn.alibaba.ir/h2/desktop/assets/images/hero/hero-e1fa22fb.webp" alt="Airplane"/>
                 </div>
+                {/* products box */}
+                <ProductsBox>
+
+                </ProductsBox>
             </div>
         </div>
     )
