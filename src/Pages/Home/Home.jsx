46b21e64d15b0dd05b4cd46a7ea63cf8753786bf
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-06 20:36:43 +0330

    product box to home development (to home component)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import ProductsBox from "../../Component/ProductsBox/ProductsBox.jsx";
+import {LuArrowLeftRight} from "react-icons/lu";
 
 export default function Home() {
     //JSX
@@ -10,8 +11,45 @@ export default function Home() {
                 </div>
                 {/* products box */}
                 <ProductsBox>
-                    <div className="w-full h-20">
+                    <div className="px-8 text-gray-600">
+                        <form action="#" onSubmit={e => e.preventDefault()}>
+                            <div>
+                                <div className="product-select-box">
+                                    <select name="Round trip or one way?" className="p-[2px] outline-none">
+                                        <option value="false">یک طرفه</option>
+                                        <option value="true">رفت و برگشت</option>
+                                    </select>
+                                </div>
 
+                                <div className="flex items-center">
+                                    <div>
+                                        <div>
+                                            <input type="text" placeholder="مبدا (شهر)"/>
+                                        </div>
+                                        <span>
+                                            <LuArrowLeftRight/>
+                                        </span>
+                                        <div>
+                                            <input type="text" placeholder="مقصد (شهر)"/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <div>
+                                                <input type="text" placeholder="تاریخ رفت"/>
+                                            </div>
+                                            <div>
+                                                <input type="text" placeholder="تاریخ بازگشت"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="تعداد مسافر"/>
+                                    </div>
+                                    <button type="submit">جستجو</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </ProductsBox>
             </div>
