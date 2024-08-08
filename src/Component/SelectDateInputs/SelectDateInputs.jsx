ce64f65ee6a35cce7b040168fd18813ce4b985ea
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-09 02:17:25 +0330

    dev

--- src/Component/SelectDateInputs/SelectDateInputs.jsx
+++ src/Component/SelectDateInputs/SelectDateInputs.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import DatePicker, {DateObject} from "react-multi-date-picker";
+import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 
@@ -8,19 +8,42 @@ export default function SelectDateInputs() {
     const [departureDate, setDepartureDate] = useState(null);
     const [returnDate, setReturnDate] = useState(null);
 
+
     //JSX
     return (
         <div className="product-box-input-wrapper">
             <div className="p-2 border-l border-solid border-gray-300">
-                <DatePicker value={departureDate} onChange={setDepartureDate}
-                            calendar={persian} locale={persian_fa} placeholder="تاریخ رفت"
-                            inputClass="w-[136px] h-[30px] p-0 m-0 border-0 outline-none"
+                <DatePicker value={departureDate} onChange={date => setDepartureDate(date)}
+                            render={(value, openCalendar) => {
+                                return (
+                                    <input
+                                        value={value}
+                                        onClick={openCalendar}
+                                        readOnly
+                                        className="w-[136px] h-[30px] p-0 m-0 border-0 outline-none"
+                                        placeholder="تاریخ رفت"
+                                    />
+
+                                )
+                            }}
+                            calendar={persian} locale={persian_fa}
                 />
             </div>
             <div className="p-2">
-                <DatePicker value={returnDate} onChange={setReturnDate}
-                            calendar={persian} locale={persian_fa} placeholder="تاریخ بازگشت"
-                            inputClass="w-[136px] h-[30px] p-0 m-0 border-0 outline-none"
+                <DatePicker value={returnDate} onChange={date => setReturnDate(date)}
+                            render={(value, openCalendar) => {
+                                return (
+                                    <input
+                                        value={value}
+                                        onClick={openCalendar}
+                                        readOnly
+                                        className="w-[136px] h-[30px] p-0 m-0 border-0 outline-none"
+                                        placeholder="تاریخ بازگشت"
+                                    />
+
+                                )
+                            }}
+                            calendar={persian} locale={persian_fa}
                 />
             </div>
         </div>
