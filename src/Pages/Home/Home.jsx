6013571a870a5fc12a20cee8270c9c1f2f2e64f2
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-10 20:02:33 +0330

    responsive...

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className="container">
             <div>
-                <div>
+                <div className="hidden md:block">
                     <img src="https://cdn.alibaba.ir/h2/desktop/assets/images/hero/hero-e1fa22fb.webp" alt="Airplane"/>
                 </div>
                 {/* products Details box */}
