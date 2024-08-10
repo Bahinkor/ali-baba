aad17c3eeaf0284af0f445b10c01f71e7724d85f
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-08-10 23:49:40 +0330

    Development mobile bottom menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {useRoutes} from "react-router-dom";
 import routes from "./routes/routes.jsx";
 import Header from "./Component/Header/Header.jsx";
+import MobileBottomMenu from "@/Component/MobileBottomMenu/MobileBottomMenu.jsx";
+
 
 export default function App() {
     //router
@@ -13,6 +15,11 @@ export default function App() {
 
             <main className="mt-[57px]">
                 {router}
+
+
+                {/* mobile bottom menu */}
+                <MobileBottomMenu/>
+
             </main>
 
             {/* footer */}
---
+++ src/Component/MobileBottomMenu/MobileBottomMenu.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import {GoHomeFill} from "react-icons/go";
+import {FaRegUserCircle, FaSuitcase} from "react-icons/fa";
+import {IoMdNotificationsOutline} from "react-icons/io";
+import {FiPlusCircle} from "react-icons/fi";
+import "./MobileBottomMenu.css";
+
+export default function MobileBottomMenu() {
+    //JSX
+    return (
+        <div
+            className="absolute bottom-0 left-0 right-0 block md:hidden bg-white pt-2 pb-1 border-t border-solid border-gray-300">
+            <div className="container">
+                <div
+                    className="mobile-bottom-menu flex items-center justify-between gap-2 [&>div>a]:flex [&>div>a]:flex-col [&>div>a]:items-center [&>div>a>span]:text-xs">
+                    <div>
+                        <NavLink to="/">
+                            <GoHomeFill/>
+                            <span>خانه</span>
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="*">
+                            <FiPlusCircle/>
+                            <span>پلاس</span>
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="*">
+                            <FaSuitcase/>
+                            <span>سفر های من</span>
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="*">
+                            <IoMdNotificationsOutline/>
+                            <span>اعلان ها</span>
+                        </NavLink>
+                    </div>
+                    <div>
+                        <NavLink to="*">
+                            <FaRegUserCircle/>
+                            <span>حساب کاربری</span>
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
